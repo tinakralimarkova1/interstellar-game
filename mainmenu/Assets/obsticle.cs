b71efd655458a784f2c2e674ad81afd66c254be5
tinakralimarkova1 <tinathetinytuna@gmail.com>
2022-01-20 11:05:10 -0800

    Added Game over functions

---
+++ mainmenu/Assets/obsticle.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class obsticle : MonoBehaviour
+{
+    private GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "border")
+        {
+            Destroy(this.gameObject);
+        }
+
+        else if(collision.tag == "Player")
+        {
+            Destroy(player.gameObject);
+        }
+    }
+}
