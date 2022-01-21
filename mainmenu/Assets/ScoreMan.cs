4b574c8d134d627ac79ea2880a6a46a94088c6ea
tinakralimarkova1 <tinathetinytuna@gmail.com>
2022-01-21 10:27:17 -0800

    Added a score

---
+++ mainmenu/Assets/ScoreMan.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreMan : MonoBehaviour
+{
+    public Text scoreText;
+    private float score;
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(GameObject.FindGameObjectWithTag("Player") != null)
+        {
+            score += 10 * Time.deltaTime;
+            scoreText.text = ((int)score).ToString();
+        }
+    }
+}
